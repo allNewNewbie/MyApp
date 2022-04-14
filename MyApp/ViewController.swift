baaf48bb4d28a714e08bcab7f558fcdb34e0d2d2
zzz <>
2022-04-14 22:05:40 +0800

    init

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+
+import UIKit
+import MyFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print(MyFwClass().method())
+    }
+
+
+}
+
